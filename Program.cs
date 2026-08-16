d3546c496d6d42b913a362c22056ded949554bab
andreh <andredalloul09@gmail.com>
2026-08-16 12:09:17 +0300

    fixing auth

--- Program.cs
+++ Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddSingleton<IMongoDatabase>(database);
 
 // Register Custom Auth Service
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<IPostService, PostService>();
 builder.Services.AddScoped<IFollowService, FollowService>();
 // builder.Services.AddSingleton<IEmailService, EmailService>();
--- obj/Debug/net10.0/Repflow.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/Repflow.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repflow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef41a9aa2a81c97da25ab24f850a0222e173b5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ebbafbf2bc3b75b9ffde425e04ce9481727340")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repflow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repflow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
