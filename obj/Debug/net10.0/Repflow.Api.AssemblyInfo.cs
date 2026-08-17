28fa9488b796941d7313e48ca71091440e5f942f
DaniNaddaf1 <naddafdani2@gmail.com>
2026-08-17 23:41:15 +0300

    ..

--- obj/Debug/net10.0/Repflow.Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/Repflow.Api.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repflow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea73d5c2b72a1a98c9e6fd6c2549a45e4b0a6377")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ba196865cccea2b9150b47c562562ae2e9fdddb")]
+>>>>>>> 7e31a93 (..)
 [assembly: System.Reflection.AssemblyProductAttribute("Repflow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repflow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
